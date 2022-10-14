698b2557d3e6f98f729127fa711feac96c3eb90d
Rafael Watanabe <rafael_jp5@live.com>
2022-10-14 18:55:05 -0300

    Desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Runtime.ExceptionServices;
+using System.Linq.Expressions;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -16,15 +18,17 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            //Iplementado!
+            if (Suite.Capacidade >= hospedes.Count  )
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+               // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+              //Iplementado!
+              throw new Exception("Suite selecionada não suporta a quantidade de hóspedes desta reserva.");
+
             }
         }
 
@@ -36,23 +40,28 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+             //Iplementado!
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+             //Iplementado!
+           
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+           
 
+           
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+              //Iplementado!
+            if (DiasReservados >= 10)
             {
-                valor = 0;
-            }
+                valor = valor - (valor * 0.1M);
+               
+            } 
+
+
 
             return valor;
         }
